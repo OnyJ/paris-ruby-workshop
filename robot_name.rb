07d22586c3145ee84609cbff1f7ab20331e7c00b
Laurent-Ch <chamouleau.laurent@gmail.com>
2022-05-11 21:01:52 +0200

    Check if name is unique

--- robot_name.rb
+++ robot_name.rb
@@ -7,29 +7,34 @@ class Robot
 
   def initialize(name = generate_name)
     @name = name
+    validate_name
   end
 
   def generate_name
     name = ""
     2.times { name += 'A'.upto('Z').to_a.sample }
     3.times { name += '0'.upto('9').to_a.sample }
-    if name_is_new(name)
-      puts "im ok"
-      Robot.all_names << name
-      return name
+    name
+  end
+
+  def validate_name
+    if name_is_new?
+      Robot.all_names << @name
+    else
+      @name = generate_name
+      validate_name
     end
   end
 
   private
 
-  def name_is_new(name)
-    !Robot.all_names.include?(name)
-    puts !Robot.all_names.include?(name)
+  def name_is_new?
+    !Robot.all_names.include?(@name)
   end
+
 end
 
-a = Robot.new
-b = Robot.new
-puts a.name 
+a = Robot.new("Babar")
+b = Robot.new("Babar")
 puts Robot.all_names
 
